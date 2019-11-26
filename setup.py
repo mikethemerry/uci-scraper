d470a2035b4cda322219429f85514e2dacce5a7a
Mike Merry <merry.mike@gmail.com>
2019-11-27 10:53:24 +1300

    Initial setup from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='uci-scraper',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Scraper to consolidate the UCI Datasets',
+    author='Mike Merry',
+    license='MIT',
+)
